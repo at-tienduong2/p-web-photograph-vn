5b4789629e5d4094ff59ebdcf4c6daed59190061
Trung Nguyen <leshin2015@gmail.com>
2018-03-15 01:21:39 +0700

    create logo header

--- src/components/common/header-page.vue
+++ src/components/common/header-page.vue
@@ -1,16 +1,16 @@
 <template>
   <header>
-      <p>
-          trung
-          {{ title }}
-      </p>
+    <div class="container">
+      <h1>
+        <a href="http://">về với yêu thương</a>
+      </h1>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
   name: "header-page",
-  props: ["title"]
 };
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HomePage from '../components/pages/home.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Home page',
+    component: HomePage
+  }]
 })
