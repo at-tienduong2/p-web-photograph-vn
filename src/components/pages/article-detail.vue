70452b16969283a8604b704ce713a5355f8eb5ac
Tien Duong <tien.duong@asiantech.vn>
2018-07-27 17:39:18 +0700

    create article detail

---
+++ src/components/pages/article-detail.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="article-detail container">
+    <div class="row">
+      <div class="col-md-12 mg-t--8 article-banner">
+        <div class="article-img">
+          <a class="img-link" href="#">
+            <img class="img" src="../../assets/images/media_160.jpg" alt="post-media">
+          </a>
+          <span class="overlay"></span>
+        </div>
+        <div class="article-header-text pd-l--8 pd-b--8">
+          <ul class="breadcrumb-list">
+            <li class="breadcrumb-item"><a class="link" href="#">art</a></li>
+            <li class="breadcrumb-item active"><a class="link" href="#">Time to gear up</a></li>
+          </ul>
+          <h1 class="title-1 font-60 mg-b--4">time to gear up</h1>
+          <div class="article-meta d-flex">
+            <div class="art-avater mg-r--4">
+              <img class="avatar" src="../../assets/images/avatar.jpeg" alt="">
+            </div>
+            <div class="art-author line-h40 mg-r--2">
+              <span class="text-by">By</span>
+              <a href="https://www.facebook.com/jollydiem">Jolly Diem</a>
+              <span class="date">July 27, 2018</span>
+            </div>
+            <div class="total-view line-h40">
+              <i class="icon-eye"></i>
+              <span>3,549 views</span>
+              <i class="ion-chatbubble mg-l--5"></i><span>0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p>ngoc tien</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'article-detail'
+}
+</script>
+
+<style>
+
+</style>
