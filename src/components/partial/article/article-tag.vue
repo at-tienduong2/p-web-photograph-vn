09c7c31dfdc943148e02c12e0308052404f04561
at-quando <quan.do@asiantech.vn>
2018-07-27 17:29:39 +0700

    grid

--- src/components/partial/article/article-tag.vue
+++ src/components/partial/article/article-tag.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="article-tag">
-    
+    <span v-bind:key="item" v-for="item in tag">
+      <el-button class="tag tag-size" :style="{'background-color': colorArr[Math.floor(Math.random() * colorArr.length)]}">{{item}}</el-button>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'article-tag',
+  props: ['tag'],
   data () {
     return {
+      colorArr: [
+        '#677e52',
+        '#374140',
+        '#917a56',
+        '#bd4932',
+        '#1b5244',
+        '#105b63'
+      ]
     }
-  },
-  mounted () {
   }
 }
 </script>
--- src/components/partial/carousel.vue
+++ src/components/partial/carousel.vue
@@ -4,7 +4,7 @@
       <div class="image-box">
         <img src="https://static.highsnobiety.com/wp-content/uploads/2017/07/21120055/sneaker-photos-instagram-supreme-louis-vuitton-yeezy-boost-custom-01-480x320.jpg">
         <div class="slider-content">
-          <el-button class="tag tag-size">Success</el-button>
+          <tag :tag="['Success', 'Fail']"/>
           <h2 class="title-1">Champagne Jell-oshort Shots Are Full</h2>
           <p class="desc-1">DO HONG QUAN - June 06,2018 </p>
         </div>
@@ -14,7 +14,7 @@
       <div class="image-box">
         <img src="https://cdn-images-1.medium.com/max/1732/1*tHfNH7iOUJHznZutbllKPg.jpeg">
         <div class="slider-content">
-          <el-button class="tag tag-size">Success</el-button>
+          <tag :tag="['Success', 'Fail']"/>
           <h2 class="title-1">Champagne Jell-oshort Shots Are Full</h2>
           <p class="desc-1">DO HONG QUAN - June 06,2018 </p>
         </div>
@@ -24,7 +24,7 @@
       <div class="image-box">
         <img src="http://static3.uk.businessinsider.com/image/546baf52dd0895d4088b463a/the-best-instagram-accounts-to-follow.jpg">
         <div class="slider-content">
-          <el-button class="tag tag-size">Success</el-button>
+          <tag :tag="['Success', 'Fail']"/>
           <h2 class="title-1">Champagne Jell-oshort Shots Are Full</h2>
           <p class="desc-1">DO HONG QUAN - June 06,2018 </p>
         </div>
@@ -34,7 +34,7 @@
       <div class="image-box">
         <img src="https://www.booooooom.com/wp-content/uploads/2013/10/instagram-bestof-05.jpg">
         <div class="slider-content">
-          <el-button class="tag tag-size">Success</el-button>
+          <tag :tag="['Success', 'Fail']"/>
           <h2 class="title-1">Champagne Jell-oshort Shots Are Full</h2>
           <p class="desc-1">DO HONG QUAN - June 06,2018 </p>
         </div>
@@ -59,14 +59,6 @@ export default {
     }
   },
   mounted () {
-    // $('.variable-width').slick({
-    //   dots: true,
-    //   infinite: true,
-    //   speed: 300,
-    //   slidesToShow: 1,
-    //   centerMode: true,
-    //   variableWidth: true
-    // })
     $('.responsive').slick({
       dots: true,
       infinite: false,
@@ -97,48 +89,8 @@ export default {
             slidesToScroll: 1
           }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
       ]
-    });
-    var tagList = document.getElementsByClassName('tag')
-    for (var item of tagList) {
-      item.style.backgroundColor = this.colorArr[Math.floor(Math.random()*this.colorArr.length)]
-    }
+    })
   }
 }
-// <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-//     <div class="multiple-items">
-//       <div class="carousel-item">
-//         <div class="image-holder" style="background: url('http://themewinter.com/WP/blogline/wp-content/uploads/2016/02/bg1.jpg')"></div>
-//         <div class="slider-content">
-//           <h2 class="entry-title">123</h2>
-//           <a class="title-font slider btn btn-primary"></a>
-//         </div>
-//       </div>
-//       <div class="carousel-item">
-//         <div class="image-holder" style="background: url('http://themewinter.com/WP/blogline/wp-content/uploads/2016/02/bg3.jpg')"></div>
-//         <div class="slider-content">
-//           <h2 class="entry-title">123</h2>
-//           <a class="title-font slider btn btn-primary"></a>
-//         </div>
-//       </div>
-//       <div class="carousel-item">
-//         <div class="image-holder" style="background: url('http://themewinter.com/WP/blogline/wp-content/uploads/2016/02/post71.jpg')"></div>
-//         <div class="slider-content">
-//           <h2 class="entry-title">123</h2>
-//           <a class="title-font slider btn btn-primary"></a>
-//         </div>
-//       </div>
-//     </div>
-//     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-//       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-//       <span class="sr-only">Previous</span>
-//     </a>
-//     <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-//       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-//       <span class="sr-only">Next</span>
-//     </a>
-//   </div>
 </script>
