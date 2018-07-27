61a4dbb0a4381b77069611dae7ad5f627e3f165a
at-quando <quan.do@asiantech.vn>
2018-07-27 08:27:43 +0700

    articles

---
+++ src/components/partial/article/article-tag.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="article-tag">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'article-tag',
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  }
+}
+</script>
